b2d754ab9759915f65a255c30f03bf26f8f6075a
Agnese Salutari <agneses92@hotmail.it>
2019-09-29 16:30:38 +0200

    Update exercise1.ino

--- exercise1/exercise1.ino
+++ exercise1/exercise1.ino
@@ -37,22 +37,20 @@ See the License for the specific language governing permissions and limitations 
 
 // Global Variables:
 uint8_t state;
-bool stateChange; // true when state is changed, false otherwise
+unsigned long lastSingleTimeMillis;
+
+// Global Control Variables:
+bool stateChanged; // true when state is changed, false otherwise
 bool upPressed; // true when pressed, false otherwise
 bool downPressed; // true when pressed, false otherwise
 bool backPressed; // true when pressed, false otherwise
 bool motorAuth; // true when motor is authorized to move, false otherwise
-uint8_t lastSingleTimeSec;
 
 
 void setup() {
   noInterrupts();
-  stateChange = false;
-  motorAuth = false;
-  upPressed = false;
-  downPressed = false;
-  backPressed = false;
-  lastSingleTimeSec = 0;
+  cleanControlVariables();
+  lastSingleTimeMillis = 0;
   pinMode(UP, INPUT_PULLUP);
   pinMode(DOWN, INPUT_PULLUP);
   pinMode(BACK, INPUT_PULLUP);
@@ -66,8 +64,65 @@ void setup() {
 }
 
 void loop() {
-  changeState();
-  stateChange = false;
+  updateState();
+  cleanControlVariables();
+  performState();
+}
+
+void cleanControlVariables(){
+  stateChanged = false;
+  motorAuth = false;
+  upPressed = false;
+  downPressed = false;
+  backPressed = false;
+}
+
+void updateState(){
+  noInterrupts();
+  switch(state){
+    case(OFF):
+      if(upPressed){
+        state = PULSED;
+      }
+      else if(downPressed){
+        state = SINGLE;
+      }
+      else if(backPressed){
+        state = SPRINKLING;
+      }
+      break;
+    case(SINGLE):
+      state = OFF;
+      break;
+    case(SPRINKLING):
+      state = OFF;
+      break;
+    case(PULSED):
+      if(upPressed){
+        state = SLOW;
+      }
+      else if(downPressed){
+        state = OFF;
+      }
+      break;
+    case(SLOW):
+      if(upPressed){
+        state = FAST;
+      }
+      else if(downPressed){
+        state = PULSED;
+      }
+      break;
+    case(FAST):
+      if(downPressed){
+        state = SLOW;
+      }
+      break;
+  }
+  interrupts();
+}
+
+void performState(){
   switch(state){
     case OFF:
       off();
@@ -90,57 +145,14 @@ void loop() {
   }
 }
 
-void cleanButtons(){
-  upPressed = false;
-  downPressed = false;
-  backPressed = false;
-}
-
-void changeState(){
-  switch(state){
-    case(OFF):
-      if(upPressed){
-        state = PULSED;
-      }
-      else if(downPressed){
-        state = SINGLE;
-      }
-      else if(backPressed){
-        state = SPRINKLING;
-      }
-    case(SINGLE):
-      state = OFF;
-    case(SPRINKLING):
-      state = OFF;
-    case(PULSED):
-      if(upPressed){
-        state = SLOW;
-      }
-      else if(downPressed){
-        state = OFF;
-      }
-    case(SLOW):
-      if(upPressed){
-        state = FAST;
-      }
-      else if(downPressed){
-        state = PULSED;
-      }
-    case(FAST):
-      if(downPressed){
-        state = SLOW;
-      }
-  }
-}
-
 void upInterrupt(){
   upPressed = true;
-  stateChange = true;
+  stateChanged = true;
 }
 
 void downInterrupt(){
   downPressed = true;
-  stateChange = true;
+  stateChanged = true;
 }
 
 void goMotor1Round(uint8_t vel){
@@ -155,17 +167,17 @@ void off(){
   state = OFF;
   motorAuth = false;
   goMotor1Round(SLOW_MOTOR); // To go in Home Position by default.
-  while((!stateChange) && (!backPressed)){
+  while((!stateChanged) && (!backPressed)){
     if(digitalRead(BACK)){
       backPressed = true;
-      stateChange = true;
+      stateChanged = true;
     }
   }
 }
 
 void single(){
   state = SINGLE;
-  lastSingleTimeSec = (uint8_t) (millis() / 1000);
+  lastSingleTimeMillis = millis();
   motorAuth = true;
   goMotor1Round(FAST_MOTOR); 
 }
@@ -173,7 +185,7 @@ void single(){
 void sprinkling(){
   state = SPRINKLING;
   digitalWrite(SPRINKLER, HIGH);
-  for(uint8_t i = 0; (i < SPRINKLING_ROUNDS) && (!stateChange); i++){
+  for(uint8_t i = 0; (i < SPRINKLING_ROUNDS) && (!stateChanged); i++){
     motorAuth = true;
     goMotor1Round(FAST_MOTOR);
   }
@@ -182,21 +194,21 @@ void sprinkling(){
 
 void pulsed(){
   state = PULSED;
-  uint8_t deltaT = STANDARD_PULSE_T_SEC;
-  if(lastSingleTimeSec < deltaT){
-    deltaT = lastSingleTimeSec;
+  unsigned long startTimeMillis = millis();
+  unsigned long deltaTMillis = STANDARD_PULSE_T_SEC * 1000;
+  if(lastSingleTimeMillis < deltaTMillis){
+    deltaTMillis = lastSingleTimeMillis;
   }
-  deltaT *= 1000; // [ms]
-  while(!stateChange){
+  while(!stateChanged){
     motorAuth = true;
     goMotor1Round(SLOW_MOTOR);
-    delay(deltaT);
+    while((!stateChanged) && (millis() - startTimeMillis <= deltaTMillis));
   }
 }
 
 void slow(){
   state = SLOW;
-  while(!stateChange){
+  while(!stateChanged){
     motorAuth = true;
     goMotor1Round(SLOW_MOTOR);
   }
@@ -204,7 +216,7 @@ void slow(){
 
 void fast(){
   state = FAST;
-  while(!stateChange){
+  while(!stateChanged){
     motorAuth = true;
     goMotor1Round(FAST_MOTOR);
   }
